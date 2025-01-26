7581ba2294a016f75481d987d51acd6f74393f9b
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-26 18:55:24 -0500

    Set up models.py with main schema

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    enrollment_date = models.DateField()
+
+class Professor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    credits = models.IntegerField()
+
+class Section(models.Model):
+    SEMESTERS = {
+        "fall": "Fall",
+        "spring": "Spring",
+        "summer": "Summer",
+    }
+    course = models.ForeignKey(Course, related_name="sections", related_query_name="section", on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, related_name="sections", related_query_name="section", on_delete = models.CASCADE)
+    building = models.CharField(max_length=30, null=True)
+    room = models.CharField(max_length=10, null=True)
+    semester_enrolled = models.CharField(max_length=10, choices=SEMESTERS)
+    year_enrolled = models.IntegerField()
+    students = models.ManyToManyField(Student, related_name="sections", related_query_name="section")
+
+class Schedule(models.Model):
+    section = models.ForeignKey(Section, related_name="schedules", related_query_name="schedule", on_delete=models.CASCADE)
+    class_day = models.CharField(max_length=10)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
