c5918f32e086725010b5b8e21bbd3afda0ffff24
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-27 16:41:27 -0500

    Implemented basic view and template to view course info

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Student, Professor, Course, Section, Schedule
 
 # Create your views here.
+def course_data(request):
+    sections = Section.objects.select_related('course', 'professor')
+    sections_info = []
+    for section in sections:
+        course_section = f"{section.course.course_name} - Section {section.id}\n"
+        location = f"Location: {section.building} - room {section.room}\n"
+        professor = f"Professor: {section.professor.first_name} {section.professor.last_name}\n"
+        schedules = f"Section Schedule"
+        for schedule in section.schedules.all():
+            schedules += f"\n  {schedule.class_day}: {schedule.start_time} - {schedule.end_time}"
+        students = f"\nStudents Registered"
+        for student in section.students.all():
+            students += f"\n  {student.id}. {student.first_name} {student.last_name}"
+        sections_info.append(course_section + location + professor + schedules + students + "\n")
+
+    return render(request, "index.html", {"sections_info": sections_info})
