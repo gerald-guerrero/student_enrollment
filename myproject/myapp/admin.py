a60f27f03b54580bcf284c6c98f42cad91154fcb
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-26 20:35:29 -0500

    register models in admin.py

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Student, Professor, Course, Section, Schedule
 
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'major', 'enrollment_date')
+    search_fields = ('first_name', 'last_name', 'major', 'enrollment_date')
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'department')
+    search_fields = ('first_name', 'last_name', 'department')
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_name', 'credits')
+    search_fields = ('course_name', 'credits')
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('course', 'professor', 'building', 'room', 'semester_enrolled', 'year_enrolled')
+    search_fields = ('course', 'professor', 'building', 'room', 'semester_enrolled', 'year_enrolled')
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('section', 'class_day', 'start_time', 'end_time')
+    search_fields = ('section', 'class_day', 'start_time', 'end_time')
+
