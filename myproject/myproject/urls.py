ac330dc14df2f913bde7f87549f10f31e83d2728
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-31 00:37:14 -0500

    Create student_records app and setup list cbv

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.course_data, name='course_data'),
+    path('', views.course_data, name='homepage'),
+    path('students/', include('student_records.urls')),
 ]
---
+++ myproject/student_records/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from myapp.models import Student
+
+# Create your views here.
+class StudentListView(ListView):
+    model = Student
+    template_name = 'student_records/student_list.html'
