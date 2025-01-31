83b354df9d68de3018d0f3e7011a0c5559a28178
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-31 01:32:20 -0500

    Add update view feature for student app

--- myproject/student_records/urls.py
+++ myproject/student_records/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.StudentListView.as_view(), name='student_list'),
-    path('<int:pk>', views.StudentDetailView.as_view(), name='student_detail'),
+    path('<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
     path('create/', views.StudentCreateView.as_view(), name='student_create'),
+    path('<int:pk>/update/', views.StudentUpdateView.as_view(), name='student_update'),
 ]
--- myproject/student_records/views.py
+++ myproject/student_records/views.py
@@ -8,12 +8,16 @@ class StudentListView(ListView):
     model = Student
     template_name = 'student_records/student_list.html'
 
-class StudentDetailView(DeleteView):
+class StudentDetailView(DetailView):
     model = Student
     template_name = 'student_records/student_detail.html'
 
 class StudentCreateView(CreateView):
     model = Student
     fields = ['first_name', 'last_name', 'major', 'semester_enrolled', 'year_enrolled']
-    template_name = 'student_records/student_create.html'
+    template_name = 'student_records/student_form.html'
 
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ['first_name', 'last_name', 'major']
+    template_name = 'student_records/student_form.html'
