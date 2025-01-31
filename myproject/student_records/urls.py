f0579ae88895615fcfef675f229b88b51bc7dfeb
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-31 01:49:54 -0500

    Implement delete view feature for student_records app

--- myproject/student_records/urls.py
+++ myproject/student_records/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
     path('create/', views.StudentCreateView.as_view(), name='student_create'),
     path('<int:pk>/update/', views.StudentUpdateView.as_view(), name='student_update'),
+    path('<int:pk>/delete/', views.StudentDeleteView.as_view(), name='student_delete')
 ]
--- myproject/student_records/views.py
+++ myproject/student_records/views.py
@@ -2,22 +2,45 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from myapp.models import Student
+from django.urls import reverse_lazy
 
 # Create your views here.
 class StudentListView(ListView):
+    '''
+    Generic CBV for list all created student objects
+    '''
     model = Student
     template_name = 'student_records/student_list.html'
 
 class StudentDetailView(DetailView):
+    '''
+    CBV that provides details on a specific student object
+    '''
     model = Student
     template_name = 'student_records/student_detail.html'
 
 class StudentCreateView(CreateView):
+    '''
+    Handles the creation of new students by auto generating a form based on the student model
+    '''
     model = Student
     fields = ['first_name', 'last_name', 'major', 'semester_enrolled', 'year_enrolled']
     template_name = 'student_records/student_form.html'
 
 class StudentUpdateView(UpdateView):
+    '''
+    Handles updating field of a specific student object except for the semester and year fields
+    as those should not be updated
+    '''
     model = Student
     fields = ['first_name', 'last_name', 'major']
     template_name = 'student_records/student_form.html'
+
+class StudentDeleteView(DeleteView):
+    '''
+    Handles deletion of a given student object and redirects to the student list view if the 
+    deletion goes through
+    '''
+    model = Student
+    template_name = 'student_records/student_confirm_delete.html'
+    success_url = reverse_lazy('student_list')
