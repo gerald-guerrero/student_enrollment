21495e9018b75d2e766b1718ad9f8f9db736c65c
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-02-01 01:27:38 -0500

    create section_records app for section views and section enrollment

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'student_records',
+    'section_records',
 ]
 
 MIDDLEWARE = [
---
+++ myproject/section_records/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SectionRecordsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'section_records'
